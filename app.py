e7e1ff49f4608785898251d89fb7e330390f8579
Ramesh C <108140367+Rameshcm581@users.noreply.github.com>
2025-03-13 22:29:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -194,23 +194,8 @@ def logout():
     flash("You have been logged out.", "info")
     return redirect(url_for('login'))
 
-files = {
-    '16rCpiE5gX4TzgrQ2gnNG5wkHnJfToRBC': 'gbm_drug_recommendation_model.pkl',
-    '14SLBek0cIV-wGh0E2vObMKPEardIEQjx': 'label_encoder.pkl'
-}
-
-def download_files():
-    for file_id, filename in files.items():
-        if not os.path.exists(filename):
-            url = f'https://drive.google.com/uc?id={file_id}'
-            gdown.download(url, filename, quiet=False)
-            print(f"Downloaded {filename}")
-        else:
-            print(f"{filename} already exists")
-
 # Run the Application
 if __name__ == '__main__':
-    download_files()
     with app.app_context():
         db.create_all() 
     app.run(debug=True) 
